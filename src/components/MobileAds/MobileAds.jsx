016e57cf0c55f8d36c97451050a6b50310b743ba
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-03-11 23:27:38 +0800

    mobile ads component added

--- src/components/MobileAds/MobileAds.jsx
+++ src/components/MobileAds/MobileAds.jsx
@@ -17,9 +17,35 @@ const backgroundBanner = {
 const MobileAds = () => {
   return (
     <>
-      <div style={backgroundBanner}>
+      <div style={backgroundBanner} className="py-14">
         <div className="container">
-          <div className="grid"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-4">
+            <div className="space-y-6 max-w-xl mx-auto">
+              {/*Text Content */}
+              <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-3 text-white shadow-md">
+                For online delivery, Espresso&Tea Co. App is now available for
+                Android and IOS
+              </h1>
+              {/* Logo Content */}
+              <div className="flex flex-wrap justify-center sm:justify-start items-center">
+                <a href="#">
+                  <img
+                    src={AppleStore}
+                    alt="Apple IOS logo"
+                    className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  />
+                </a>
+                <a href="#">
+                  <img
+                    src={GooglePlay}
+                    alt="Google Play Store logo"
+                    className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                  />
+                </a>
+              </div>
+            </div>
+            <div></div>
+          </div>
         </div>
       </div>
     </>
