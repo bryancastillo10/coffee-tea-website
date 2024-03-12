5e660340ab1a3bea9a7cddb40e55beac6c5b9c58
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-03-12 23:29:13 +0800

    initiate footer

---
+++ src/components/Footer/FootList.js
@@ -0,0 +1,20 @@
+const FootList = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#",
+  },
+  {
+    title: "Contact",
+    link: "/#",
+  },
+  {
+    title: "Blogs",
+    link: "/#",
+  },
+];
+
+export default FootList;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import FootList from "./FootList.js";
+import FooterImg from "../../assets/webpage/coffee-footer.jpg";
+
+const backgroundFooter = {
+  backgroundImage: `url(${FooterImg})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  minheight: "400px",
+  width: "100%",
+};
 
 const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <div style={backgroundFooter} className="text-white mt-10 py-1">
+      <div className="bg-tertiary/20 min-h-[400px] pt-1">
+        <div className="container grid md:grid-cols-3 pb-20">
+          {/* Company Details */}
+          <div className="">
+            <a href="#">Espresso & Tea Co.</a>
+            <p></p>
+          </div>
+          {/* Footer Links */}
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
