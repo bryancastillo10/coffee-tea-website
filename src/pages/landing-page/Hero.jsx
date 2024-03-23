1fe385a51819f0e387132dc8b6b5fe92f7463d8b
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-03-23 14:43:13 +0800

    remove animation for speed rendering

--- src/pages/landing-page/Hero.jsx
+++ src/pages/landing-page/Hero.jsx
@@ -12,15 +12,14 @@ const Hero = () => {
             <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold">
               Brewing up premium
               <span
-                data-aos="zoom-out"
-                data-aos-delay="300"
+                
                 className="text-primary font-cursiveTwo sm:mr-3"
               >
                 coffee and tea
               </span>
               to freshen up your day
             </h1>
-            <div data-aos="fade-up" data-aos-delay="400">
+            <div>
               <button className="mt-5 bg-gradient-to-r from-primary to-secondary border-2 border-primary rounded-full px-4 py-2 text-white hover:scale-10 duration-200">
                 Premium Delights
               </button>
@@ -28,7 +27,7 @@ const Hero = () => {
           </div>
           {/* Image  Content Section */}
           <div
-            data-aos="zoom-in"
+           
             className="min-h-[450px] flex justify-center items-center order-1 sm:order-2 relative"
           >
             <img
@@ -37,13 +36,13 @@ const Hero = () => {
               className="w-[300px] sm:w-[450px] sm:scale-110 mx-auto spin"
             />
             <div
-              data-aos="fade-left"
+             
               className="bg-gradient-to-r from-primary to-secondary absolute top-10 left-10 p-1 rounded-xl px-4 py-0.5"
             >
               <h1 className="text-xl">Must Try!</h1>
             </div>
             <div
-              data-aos="fade-right"
+             
               className="bg-gradient-to-r from-primary to-secondary absolute bottom-10 right-10 p-1 rounded-xl px-4 py-0.75"
             >
               <h1 className="text-xl">Premium Grade</h1>
