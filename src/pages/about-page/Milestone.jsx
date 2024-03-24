f154bf62ef14a2f75f36e6d23c686d40d15afc18
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-03-24 08:33:08 +0800

    initiate milestone part

--- src/pages/about-page/Milestone.jsx
+++ src/pages/about-page/Milestone.jsx
@@ -2,7 +2,8 @@ import milestoneElement from "src/lists/milestoneData.js";
 
 const Milestone = () => {
   return (<section>
-
+      <h1>Milestone Section</h1>
+      <p>Add some context</p>
   </section>
   )
 }
